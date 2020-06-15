c75e974ea9deee7766f63bbddd10695f4f7f0750
Vasudha Varadarajan <earthling2304@users.noreply.github.com>
2020-06-15 10:46:12 -0400

    adding debug pt

--- detectron/ops/add_cluster_annotation.m
+++ detectron/ops/add_cluster_annotation.m
@@ -4,6 +4,7 @@ gt_txt_path=['$VISDRONE/VisDrone2019-DET-train/annotations'];
 img_path='$VISDRONE/VisDrone2019-DET-train/images';
 gt_output_path='$VISDRONE/VisDrone2019-DET-train/annotations_add_cluster_coco_format_rename';
 gt_file_list=dir(fullfile(gt_txt_path,'*txt'));
+disp("debug 1")
 clus_top_n=10;
 dota_flag=0;
 only_cluster=0;
