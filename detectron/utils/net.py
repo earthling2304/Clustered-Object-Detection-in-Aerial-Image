93c179a4ee0539727d558e7d5dfbdc958f682cbe
Vasudha Varadarajan <earthling2304@gmail.com>
2020-06-18 18:48:25 -0400

    WS blobs debug

--- detectron/utils/net.py
+++ detectron/utils/net.py
@@ -59,9 +59,9 @@ def initialize_gpu_from_weights_file(model, weights_file, gpu_id=0):
     """
     logger.info('Loading weights from: {}'.format(weights_file))
     ws_blobs = workspace.Blobs()
+    print("WS blobs")
+    print(ws_blobs)
     src_blobs = load_object(weights_file)
-    print("SOURCE BLOBS:")
-    print(src_blobs)
     if 'cfg' in src_blobs:
         saved_cfg = load_cfg(src_blobs['cfg'])
         configure_bbox_reg_weights(model, saved_cfg)
